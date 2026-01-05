9f480a586f1f0743a0d73e22af61477ecee7007a
Olga Panayot <panayot.olga@gmail.com>
2026-01-05 14:21:02 +0200

    feat: add pandas example for writing data to Excel

---
+++ src/favorite_fruits.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = {
+    'Фрукти': ['Яблуко', 'Банан', 'Вишня'],
+    'Кількість': [10, 5, 2]
+}
+df = pd.DataFrame(data)
+
+df.to_excel('fruits.xlsx', sheet_name='Улюблені фрукти', index=False)
+
+print("Дані успішно записано у файл 'fruits.xlsx'")
